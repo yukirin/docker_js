64fea38ec6303d7e3befeb08120b2156440c0779
yukirin <standupdown@gmail.com>
2021-02-13 15:49:36 +0000

    Fix

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = 80;
-const host = '127.0.0.1';
+const host = "127.0.0.1";
 app.listen(port, host);
 
 console.log(`Running on http://${host}:${port}`);
