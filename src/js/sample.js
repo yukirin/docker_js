223be8a45a78bdf05789f9f5378ebf61ed70a592
yukirin <standupdown@gmail.com>
2021-02-22 08:57:50 +0000

    Add JSDoc

--- src/js/sample.js
+++ src/js/sample.js
@@ -1,1 +1,39 @@
-alert("sample page");
+/**
+ * Animal Class
+ *
+ * @class Animal
+ */
+class Animal {
+  /**
+   * Creates an instance of Animal.
+   * @param {String} name
+   * @memberof Animal
+   */
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+}
+
+/**
+ * Rabbit Class
+ *
+ * @class Rabbit
+ * @extends {Animal}
+ */
+class Rabbit extends Animal {
+  /**
+   * Creates an instance of Rabbit.
+   * @param {String} name
+   * @param {Number} earLength
+   * @memberof Rabbit
+   */
+  constructor(name, earLength) {
+    super(name);
+    this.earLength = earLength;
+  }
+}
+
+const rabbit = new Rabbit("White Rabbit", 10);
+alert(rabbit.name);
+alert(rabbit.earLength);
