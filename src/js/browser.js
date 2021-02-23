56a39b7d85285de1bc5c540fb501a086a31dcdc8
yukirin <standupdown@gmail.com>
2021-02-23 20:39:11 +0000

    Add MutationObserver

--- src/js/browser.js
+++ src/js/browser.js
@@ -1,1 +1,12 @@
 console.log(`innerHeight = ${window.innerHeight}`);
+
+const observer = new MutationObserver((mutationRecords) => {
+  console.log(mutationRecords);
+});
+
+observer.observe(elem, {
+  childList: true,
+  subtree: true,
+  characterData: true,
+  characterDataOldValue: true,
+});
